a4f0a8f8c2e3177158023259dbde9c556097bdbd
Alejandro-Villa <awewanwo@disroot.org>
2020-01-02 20:13:51 +0100

    TODO list added

--- src/cocinero_integral.cpp
+++ src/cocinero_integral.cpp
@@ -1,3 +1,25 @@
+/*
+ * REQUISITOS:
+ *
+ * 1.- Mostrar toda la info de una receta. (ingr, info nutri, pasos)
+ * 2.- Fusionar dos recetas en una sola.
+ *
+ * USO:
+ * $ /<path>/cocinero_integral <fichero acciones> <fichero recetas> <fichero ingre> <fichero instr>
+ *
+ * DETALLE FUNCIONAMIENTO:
+ *
+ * 1.- Mostrar todos los códigos de las recetas, con los nombres y los platos.
+ * 2.- Recoger un codigo receta y mostrar en detalle esa receta (ingre, info nutri, pasos).
+ * 3.- Se pide otro código para fusionar dos recetas.
+ * 4.- Muestra toda la info de la fusión.
+ */
+
+
+#include "recetas.h"
+#include<iostream>
+
+usign namespace std;
 
 int main() {
 	return 0;
