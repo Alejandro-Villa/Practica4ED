50e93b553d5e0d7f5d8f08134a205ac815de9d4b
Alejandro-Villa <awewanwo@disroot.org>
2020-01-03 23:58:54 +0100

    cocinero_integral v0.2

--- src/cocinero_integral.cpp
+++ src/cocinero_integral.cpp
@@ -64,34 +64,73 @@ int main(int argc, char* argv[]) {
 	}
 
 	cout << fixed;
-	cout << setw(50) << left << "Acciones localizadas en:" << setw(20) << left << KGRN << pathtoacc << RST << endl;
+	cout << setw(50) << left << "Acciones localizadas en: " << KGRN << pathtoacc << RST << endl;
 	cout << setw(30) << left << "Leyendo acciones..." << endl;
 
 	acciones accread;
 	fsacc >> accread;
 	instrucciones::acc = accread;
 
-	cout << setw(50) << left << FGRN("Acciones leídas con éxito") << endl;
-	cout << setw(50) << left << "Ingredientes localizados en:" << setw(20) << left << KGRN << pathtoingre << RST << endl;
+	cout << setw(8) << "" << setw(50) << left << FGRN("Acciones leídas con éxito") << endl;
+	cout << setw(50) << left << "Ingredientes localizados en: " << KGRN << pathtoingre << RST << endl;
 	cout << setw(30) << left << "Leyendo ingredientes..." << endl;
 
 	recetas allrecetas;
 	allrecetas.getIngredientes(fsingre);
 
-	cout << setw(50) << left << FGRN("Ingredientes leídos con éxito") << endl;
-	cout << setw(50) << left << "Recetas localizadas en:" << setw(20) << left << KGRN << pathtorecetas << RST << endl;
+	cout << setw(8) << "" << setw(50) << left << FGRN("Ingredientes leídos con éxito") << endl;
+	cout << setw(50) << left << "Recetas localizadas en: " << KGRN << pathtorecetas << RST << endl;
+	cout << setw(30) << left << "Leyendo ingredientes..." << endl;
 
 	fsrecetas >> allrecetas;
 
-	cout << setw(50) << left << "Directorio de instrucciones localizado en:" << setw(20) << left << KGRN << pathtoinst << RST << endl;
-
-
+	cout << setw(8) << "" << setw(50) << left << FGRN("Recetas leídas con éxito") << endl;
+	cout << setw(50) << left << "Directorio de instrucciones localizado en: " << KGRN << pathtoinst << RST << endl;
 	cout << endl << fixed;
 	for (auto i=allrecetas.begin(); i != allrecetas.end(); ++i) {
 		cout << setw(6) << left << FCYN("CODE: ") << setw(4) << left << (*i).getCode();
 		cout << setw(8) << left << FCYN("NOMBRE: ") << setw(35) << left << (*i).getNombre();
-		cout<< setw(7) << left <<  FCYN("PLATO: ") << setw(2) << left << (*i).getPlato() << endl;
+		cout << setw(7) << left <<  FCYN("PLATO: ") << setw(2) << left << (*i).getPlato() << endl;
 	}
+
+	cout << "Selecciona un código de receta:\t";
+	string elegido;
+	cin >> elegido;
+
+	recetas::iterator pos = allrecetas.end();
+	if((pos = allrecetas.findReceta(elegido)) == allrecetas.end()) {
+		cerr << FRED("El código introducido no es válido o no existe") << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	cout << endl << fixed;
+	cout << setw(6) << left << FCYN("CODE: ") << setw(4) << left << (*pos).getCode();
+	cout << setw(8) << left << FCYN("NOMBRE: ") << setw(35) << left << (*pos).getNombre();
+	cout << setw(7) << left <<  FCYN("PLATO: ") << setw(2) << left << (*pos).getPlato() << endl;
+
+	cout << left << UNDL("Ingredientes:") << endl;
+
+	for(auto i = (*pos).begin(); i != (*pos).end(); ++i)
+		cout << setw(8) << left << " " << (*i).first << " " << (*i).second << endl;
+
+	cout << left << UNDL("Valores Nutricionales:") << endl;
+	cout << setw(8) << left << " " << "Calorías: " << (*pos).getCalorias() << endl;
+	cout << setw(8) << left << " " << "Hidratos de Carbono: " << (*pos).getHc() << endl;
+	cout << setw(8) << left << " " << "Grasas: " << (*pos).getGrasas() << endl;
+	cout << setw(8) << left << " " << "Proteínas " << (*pos).getProteinas() << endl;
+	cout << setw(8) << left << " " << "Fibra: " << (*pos).getFibra() << endl;
+
+	string pathtorinst;
+	pathtorinst += pathtoinst;
+	pathtorinst += '/';
+	pathtorinst += (*pos).getCode();
+	pathtorinst += "m.txt";
+
+	(*pos).cargaInstrucciones(pathtorinst);
+
+	cout << left << UNDL("Pasos a Seguir:");
+
+	(*pos).muestraInstrucciones(cout);
 
 	return 0;
 }
